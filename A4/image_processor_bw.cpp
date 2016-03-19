8dbed8dc0b98ce5c405f14ee076d92280b89db78
Rafael Solorzano <rafa@solorzanoc.com>
2016-03-19 11:11:28 -0700

    Math Histogram function

--- A4/image_processor_bw.cpp
+++ A4/image_processor_bw.cpp
@@ -19,18 +19,60 @@ void process_image(PNG_Canvas_BW& image){
 	int width = image.get_width();
 	int height = image.get_height();
 	
+
+
 	//Make a new image canvas for the output to avoid conflicts
 	PNG_Canvas_BW outputImage(width,height);
 	
 	//Placeholder: invert the image
-	for (int x = 0; x < width; x++)
-		for (int y = 0; y < height; y++)
+	for (int y = 0; y < height; y++)
+		for (int x = 0; x < width; x++)
 			outputImage[x][y] = 255 - image[x][y];
 			
 			
 	//Copy the result back into the provided image
 	image = outputImage;
 }
+
+
+int match_histrograms(PNG_Canvas_BW pixels, int width,  int height, int Href[][], int nref){
+	int n = width * height;
+	int r = n/nref;
+	int h[][] = 0; //non cumulative histogram of pixels array
+	int F[256] = 0;
+	int i, j , c = 0;
+	while (i < 256) {
+		if (c<= r * Href[j]){
+			c = c + h[i];
+			F[i] = j;
+			i = i + 1;
+		} else {
+			j++;
+		}
+	}
+	return F; //integer array
+}
+
+
+
+/*
+double CDF[] (int h[]){
+	int K = h.length;
+	int n = 0;
+	for (int i = 0; i < K; i++){
+		n += h[i];
+	}
+
+	double P[] = new double [k];
+	int c = 0;
+	for (int i = 0; i < K; i++){
+		c += h[i];
+		P[i] = (double) c/n;
+	}
+	return P;
+}
+*/
+
 
 
 
