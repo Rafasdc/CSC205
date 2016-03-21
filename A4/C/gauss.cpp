8924a8d917201babc49af988f79453f0e831e5b1
Rafael Solorzano <rafa@solorzanoc.com>
2016-03-20 22:58:19 -0700

    Gaussian 5x5 blur
    Gaussian 5x5 blur now working using assignment specs

--- A4/C/gauss.cpp
+++ A4/C/gauss.cpp
@@ -21,47 +21,46 @@ void process_image(PNG_Canvas_BW& image){
 	int width = image.get_width();
 	int height = image.get_height();
 	
-	double sigma = 1;
+	double sigma = 2.5;
 	double kernel[5][5];
 	double sum = 0;
 	for (int x=0; x< 5; x++){
 		for (int y = 0; y < 5; y++){
-			kernel[x][y] = exp(-0.5 * (pow((x-2.5)/sigma, 2.0) + pow((y-2.5)/sigma,2.0)))/(2 * M_PI * sigma * sigma);
+			kernel[x][y] = exp((-(x*x)+(y*y))/(2*sigma*sigma));
+					//exp(-0.5 * (pow((x-2.5)/sigma, 2.0) + pow((y-2.5)/sigma,2.0)))/(2 * M_PI * sigma * sigma);
 			sum += kernel[x][y];
 		}
 	}
 	//double sum = 0.0;
+
 	for (int x = 0; x<5; x++){
 		for(int y = 0; y < 5; y++){
-			kernel[x][y] /= sum;
+			//kernel[x][y] /= sum;
 			printf(" %f ",kernel[x][y]);
 		}
 		printf("\n");
 	}
-	//printf("%f.5\n",sum);
 
-	int laplace[3][3] = {
-					{0, 1, 0},
-					{1,-4, 1},
-					{0, 1, 0}
-	};
+	printf("%f\n",sum);
+
+	int K = 2;
+	double s = 1.0/sum;
 
 
 	//Make a new image canvas for the output to avoid conflicts
 	PNG_Canvas_BW outputImage(width,height);
 	
-	for (int y = 1; y < height-2; y++){
-		for (int x = 1; x < width-2; x++){
+	for (int y = K; y < height-K-1; y++){
+		for (int x = K; x < width-K-1; x++){
 			int sum = 0;
-			for (int j = -1; j <= 1; j++){
-				for (int i = -1; i <= 1; i++){
+			for (int j = -K; j <= K; j++){
+				for (int i = -K; i <= K; i++){
 					int p = image.get_pixel(x+i,y+j);
-					int c = laplace[j+1][i+1];
+					int c = kernel[j+K][i+K];
 					sum += c * p;
 				}
 			}
-			//inputPixels[x][y] = F[inputPixels[x][y]];
-			int q = sum;
+			int q = round(s*sum);
 			if (q < 0) q = 0;
 			if (q > 255) q = 255;
 			outputImage.set_pixel(x,y,q);
@@ -70,12 +69,7 @@ void process_image(PNG_Canvas_BW& image){
 
 
 
-	for (int x = 0; x < width; x++){
-		for (int y = 0; y< height; y++){
-			int s = image.get_pixel(x,y) - 0.5*outputImage.get_pixel(x,y);
-			outputImage.set_pixel(x,y,s);
-		}
-	}
+
 
 
 			
